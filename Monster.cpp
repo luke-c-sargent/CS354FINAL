98916d56b615984b0c33827f4c7a76cbdc65e246
luke-c-sargent <luke.c.sargent@gmail.com>
2015-04-30 15:40:05 -0500

    various tweaks

--- Monster.cpp
+++ Monster.cpp
@@ -1,7 +1,7 @@
 #include "Monster.h"
 
 /*
-Monster should have a specified range of "sight", if player 
+Monster should have a specified range of "sight", if player
 walks into range of sight, monster will pick up speed and follow
 the player as long as the player is still in range
 */
@@ -29,6 +29,7 @@ Monster::Monster(Ogre::SceneManager* sceneMgr)
 	m_attackRange = 100.0;
 
 	m_entity = sceneMgr->createEntity("ninja.mesh");
+	m_entity->setMaterialName("Ninja");
 
 
 /*
@@ -38,7 +39,7 @@ Monster::Monster(Ogre::SceneManager* sceneMgr)
 	mAnimationState(0),
 	mEntity(0),
 	mNode(0),
-	
+
 	range(10),
 	collision(false),
 	health(100)
@@ -63,30 +64,30 @@ void Monster::initMonster(Ogre::SceneManager* smp, int spawn_point)
 	if(spawn_point == 1)
 	{
 		m_node->setPosition(m_spawnPt1);
-		
+
 		m_walkList.push_back(m_spawnPt2);
 		m_walkList.push_back(m_spawnPt3);
 		m_walkList.push_back(m_spawnPt1);
 
-	} 
+	}
 	else if (spawn_point == 2)
 	{
 		m_node->setPosition(m_spawnPt2);
-		
+
 		m_walkList.push_back(m_spawnPt3);
 		m_walkList.push_back(m_spawnPt1);
-		m_walkList.push_back(m_spawnPt2);	
+		m_walkList.push_back(m_spawnPt2);
 	}
 	else //spawn_point == 3
 	{
 		m_node->setPosition(m_spawnPt3);
-		
+
 		m_walkList.push_back(m_spawnPt1);
 		m_walkList.push_back(m_spawnPt2);
 		m_walkList.push_back(m_spawnPt3);
 	}
-	
-	
+
+
 	m_node->setScale(0.01, 0.01, 0.01);
     //Ogre::Entity* m_entity = sceneMgr->createEntity("robot.mesh");
     m_node->attachObject(m_entity);
@@ -94,12 +95,12 @@ void Monster::initMonster(Ogre::SceneManager* smp, int spawn_point)
     m_destinationVector = m_walkList.front();
 	m_walkList.pop_front();
 	m_walkList.push_back(m_destinationVector);
-	
+
 }
 
 void Monster::changeState(MONSTER_STATE state, const Ogre::FrameEvent& evt)
 {
-	
+
 	if(state == STATE_WANDER)
 	{
 		m_animState = m_entity->getAnimationState("Walk");
@@ -110,12 +111,12 @@ void Monster::changeState(MONSTER_STATE state, const Ogre::FrameEvent& evt)
 		m_animState = m_entity->getAnimationState("Idle1");
 		//zero out the m_walkList so he stops moving around
 	}
-	
+
 	//m_animState = m_entity->getAnimationState("Walk");
-	
+
 	m_animState->setLoop(true);
 	m_animState->setEnabled(true);
-	
+
 	//m_animState->addTime(evt.timeSinceLastFrame);
 
 }
@@ -132,24 +133,24 @@ void Monster::updateMonsters(const Ogre::FrameEvent& evt)
 	//check if monster reached destination
 	if(m_distance <= 0.0f)
 	{
-		
+
 		m_node->setPosition(m_destinationVector); //place monster at destination
 		m_directionVector = Ogre::Vector3::ZERO; //set to 0 so next part of path can be started
 
 		m_destinationVector = m_walkList.front(); //update monster's next destination (front of the queue)
 		m_walkList.pop_front(); //remove the destination from the queue
 		m_walkList.push_back(m_destinationVector); //push destination back at end of queue
-		
+
 		m_directionVector = m_destinationVector - m_node->getPosition(); //set new direction vector
 		m_distance = m_directionVector.normalise();
 
 		//There should be rotation code here?
-		
+
 		Ogre::Vector3 src = m_node->getOrientation()*Ogre::Vector3::UNIT_X;
 
-		if ((1.0f + src.dotProduct(m_directionVector)) < 0.0001f) 
+		if ((1.0f + src.dotProduct(m_directionVector)) < 0.0001f)
 		{
-			m_node->yaw(Ogre::Degree(180));						
+			m_node->yaw(Ogre::Degree(180));
 		}
 		else
 		{
@@ -172,7 +173,7 @@ void Monster::moveMonster()
 		{
 	    	m_animState = m_entity->getAnimationState("Walk");
    	 		m_animState->setLoop(true);
-    		m_animState->setEnabled(true);		
+    		m_animState->setEnabled(true);
 		}
 	}
 	else
@@ -192,10 +193,10 @@ bool Monster::nextLocation()
 	m_destinationVector = m_walkList.front();
 	m_walkList.pop_front();
 	m_walkList.push_back(m_destinationVector);
-	
+
 	m_directionVector = m_destinationVector - m_node->getPosition();
 	m_distance = m_directionVector.normalise();
-	
+
 	return true;
 
 
